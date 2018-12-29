b05c3e9827a774d583999d1843e4adeda88b9136
baldor12345 <baldormanayay@gmail.com>
2018-12-28 21:06:14 -0500

    baldor

--- database/migrations/2018_09_07_143902_crear_tabla_persona.php
+++ database/migrations/2018_09_07_143902_crear_tabla_persona.php
@@ -16,7 +16,7 @@ class CrearTablaPersona extends Migration
         Schema::create('persona', function (Blueprint $table) {
             $table->increments('id');
 
-            $table->string('codigo', 40);
+            $table->string('codigo', 40)->nullable();
             $table->char('dni',8)->nullable();
             $table->string('nombres', 100)->nullable();
             $table->string('apellidos', 100)->nullable();
@@ -27,8 +27,8 @@ class CrearTablaPersona extends Migration
                 $table->string('telefono_fijo_apoderado',18)->nullable();
                 $table->string('direccion_apoderado', 80)->nullable();
 
-            $table->char('sexo', 1); // M->Masculino, F->Femenino
-            $table->char('estado_civil', 1); // S->Soltero, C=>Casado, V=>Viudo, D=>Divorciado
+            $table->char('sexo', 1)->nullable(); // M->Masculino, F->Femenino
+            $table->char('estado_civil', 2)->nullable(); // S->Soltero, C=>Casado, V=>Viudo, D=>Divorciado
             $table->integer('personas_en_casa')->nullable();
             $table->string('direccion', 80)->nullable();
             $table->string('ocupacion', 80)->nullable();
@@ -36,8 +36,8 @@ class CrearTablaPersona extends Migration
             $table->string('telefono_fijo',18)->nullable();
             $table->string('telefono_movil1',18)->nullable();
             $table->string('telefono_movil2',18)->nullable();
-            $table->decimal('ingreso_personal',10,2);
-            $table->decimal('ingreso_familiar',10,2);
+            $table->decimal('ingreso_personal',10,2)->nullable();
+            $table->decimal('ingreso_familiar',10,2)->nullable();
 
             //acciones
             $table->char('tipo',2)->nullable(); // S=>socio, C=>Cliente, 2=>SC
