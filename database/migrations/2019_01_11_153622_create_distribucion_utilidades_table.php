1900c850c82683b9d909d21ba948506afbf9e1e6
baldor12345 <baldormanayay@gmail.com>
2019-01-11 15:49:16 -0500

    baldor

---
+++ database/migrations/2019_01_11_153622_create_distribucion_utilidades_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDistribucionUtilidadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('distribucion_utilidades', function (Blueprint $table) {
+            $table->increments('id');
+            //egresos
+            $table->decimal('gast_admin_acum',20,1);
+            $table->decimal('int_pag_acum',20,1);
+            $table->decimal('otros_acum',20,1);
+            //ingresos
+            $table->decimal('intereses',20,1);
+            $table->decimal('otros',20,1);
+            $table->timestamp('fechai')->nullable();
+            $table->timestamp('fechaf')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('distribucion_utilidades');
+    }
+}
