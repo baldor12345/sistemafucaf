3b7d4ca30dd8ddef272dfc9ddf9ca15a7bd8d799
baldor12345 <baldormanayay@gmail.com>
2019-01-28 15:58:59 -0500

    baldor

--- app/Http/Controllers/CertificadoController.php
+++ app/Http/Controllers/CertificadoController.php
@@ -394,8 +394,21 @@ class CertificadoController extends Controller
         $certificado = Certificado::find($id);
         $persona = Persona::find($certificado->persona_id);
 
+        $month = array(1=>'Enero',
+                        2=>'Febrero',
+                        3=>'Marzo',
+                        4=>'Abril',
+                        5=>'Mayo',
+                        6=>'Junio',
+                        7=>'Julio',
+                        8=>'Agosto',
+                        9=>'Septiembre',
+                        10=>'Octubre',
+                        11=>'Noviembre',
+                        12=>'Diciembre');
+
         $titulo = "Certificado_".$persona->nombres;
-        $view = \View::make('app.certificado.reportecertificadoPDF')->with(compact('lista', 'id', 'fecha','certificado','persona'));
+        $view = \View::make('app.certificado.reportecertificadoPDF')->with(compact('lista', 'id', 'fecha','certificado','persona','month'));
         $html_content = $view->render();
 
         PDF::SetTitle($titulo);
